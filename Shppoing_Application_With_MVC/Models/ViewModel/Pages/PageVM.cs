48e168934535e8b1922690eadae917fc05a7b5e4
Likhit Budwal <lbudwal6@gmail.com>
2018-05-30 17:19:19 -0600

    Admin Page Fully Finish

--- Shppoing_Application_With_MVC/Models/ViewModel/Pages/PageVM.cs
+++ Shppoing_Application_With_MVC/Models/ViewModel/Pages/PageVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Shppoing_Application_With_MVC.Models.ViewModel.Pages
 {
@@ -42,6 +43,7 @@ namespace Shppoing_Application_With_MVC.Models.ViewModel.Pages
 
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 3)]
+        [AllowHtml]
         public string Body { get; set; }
         public int Sorting { get; set; }
         public bool HasSlideBar { get; set; }
--- Shppoing_Application_With_MVC/Models/ViewModel/Pages/SidebarVM.cs
+++ Shppoing_Application_With_MVC/Models/ViewModel/Pages/SidebarVM.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Shppoing_Application_With_MVC.Models.ViewModel.Pages
 {
@@ -20,6 +21,7 @@ namespace Shppoing_Application_With_MVC.Models.ViewModel.Pages
 
         }
         public int Id { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
     }
 }
